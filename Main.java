8b6a49627dd8f9f1db235bfa2983ccdc50cb3da8
Dfyxy <44157914+Dfyxy@users.noreply.github.com>
2020-07-27 20:21:55 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,23 @@
+package jhz20200607;
+
+
+
+	public class Main  {
+
+	            public static void main(String[] args){
+//
+//		Jhzstudent jst=new Jhzstudent("21192716","Ñ¦ÄÈÄÈ","000000");
+//		jst.run();
+//		Jhzprofessor jpf=new Jhzprofessor("10001","ÍõÎ°","000000");		
+//		jpf.run();
+	            	MInterface mit=new MInterface();
+	            	mit.startup();
+	 
+	 }
+}
+	
+
+
+
+	   
+
